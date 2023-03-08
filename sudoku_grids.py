e92b224edf736126a493e248c8ded927cd8a37b6
Sriya Reddy Adunur <sadunur@unix1.csc.calpoly.edu>
2023-03-07 23:06:48 -0800

    Working functions to check and fill sudoku grids, as specified.

--- sudoku_grids.py
+++ sudoku_grids.py
@@ -13,7 +13,14 @@ def check_row(grid, row):
     :return: Whether or not the row has either 0 or 1 of each of the
              integers {1, ..., 9}
     """
-    pass
+    r = []
+    for element in grid[row]:
+        if element != 0:
+            if element not in r:            
+                r.append(element)
+            else:
+                return False
+    return True
 
 
 def check_col(grid, col):
@@ -26,7 +33,14 @@ def check_col(grid, col):
     :return: Whether or not the column has either 0 or 1 of each of the
              integers {1, ..., 9}
     """
-    pass
+    r = []
+    for i in range(len(grid)):
+        if grid[i][col] != 0:
+            if grid[i][col] not in r:
+                r.append(grid[i][col])
+            else:
+                return False
+    return True
 
 
 def check_subgrid(grid, row, col):
@@ -40,8 +54,110 @@ def check_subgrid(grid, row, col):
     :return: Whether or not the 3x3 subgrid containing that row and column has
              either 0 or 1 of each of the integers {1, ..., 9}
     """
-    pass
+    r = []
 
+    if row >= 0 and row < 3:
+        row = 0
+    elif row >= 3 and row < 6:
+        row = 3
+    else:
+        row = 6
+
+    if col >= 0 and col < 3:
+        col = 0
+    elif col >= 3 and col < 6:
+        col = 3
+    else:
+        col = 6     
+
+    for i in range(row, row+3):
+        for j in range(col, col+3):
+            if grid[i][j] != 0:
+                if grid[i][j] not in r:
+                    r.append(grid[i][j])
+                else:
+                    return False
+    return True
+
+def valid_grid(grid):
+    for i in range(9):
+        for j in range(9):
+            if (grid[i][j] == 0) or not check_row(grid, i) or not check_col(grid, j) or not check_subgrid(grid, i, j):
+                return False
+    return True
+
+
+
+
+def valid_num(grid, row, col):
+    i = 1
+    while i < 10:
+        grid[row][col] = i
+        if check_row(grid, row) and check_col(grid, col) and check_subgrid(grid, row, col):
+            return True
+        else:
+            i = i + 1
+    return False
+
+def replace(grid, row, col):
+    i = 1
+    while i < 10:
+        grid[row][col] = i
+        if check_row(grid, row) and check_col(grid, col) and check_subgrid(grid, row, col):
+            return i
+        else:
+            i = i + 1
+
+def valid_b(grid, row, col):
+    num = grid[row][col] 
+    i = 1 
+    while i < 10:
+        grid[row][col] = i
+        if check_row(grid, row) and check_col(grid, col) and check_subgrid(grid, row, col) and i != num:
+            grid[row][col] = num
+            return True
+        else:
+            i = i + 1
+    return False
+
+def replace_b(grid, row, col):
+    num = grid[row][col]
+    i = 1
+    while i < 10:
+        grid[row][col] = i
+        if check_row(grid, row) and check_col(grid, col) and check_subgrid(grid, row, col) and i != num:
+            return i
+        else:
+            i = i + 1
+                     
+
+def backtrack(grid, gcpy, row, col):
+    gcpy[row][col] = 0
+    if col == 0:
+        j = 8
+        i = row - 1
+    else:
+        j = col - 1
+        i = row
+    while i >= 0 and  j >= 0:
+        if grid[i][j] != 0:
+            if j == 0:
+                j = 8
+                i = i - 1
+            else:
+                j = j - 1
+        elif valid_b(gcpy, i, j):
+            gcpy[i][j] = replace_b(gcpy, i, j)
+            return gcpy
+        else:
+            gcpy[i][j] = 0
+            if j == 0:
+                j = 8
+                i = i - 1
+            else:
+                j = j - 1
+    
+    
 
 def fill_grid(grid):
     """
@@ -50,6 +166,22 @@ def fill_grid(grid):
 
     :param grid: A 9x9 list of lists of integers
     :return: The grid, such that each row, column, and 3x3 subgrid has exactly
-             1 of each of the integers {1, ..., 9}
-    """
-    pass
+             1 of each of the integers {1, ..., 9} 
+  
+    """ 
+    gcpy = [row.copy() for row in grid] 
+    row = 0
+    col = 0
+    while row < 9 and col < 9:
+        if grid[row][col] != 0:
+            col = col
+        elif valid_num(gcpy, row, col):
+            gcpy[row][col] = replace(gcpy, row, col)
+        else:
+            gcpy = backtrack(grid, gcpy, row, col)
+        if col == 8:
+            col = 0
+            row = row + 1
+        else:
+            col = col + 1
+    return gcpy
