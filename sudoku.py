3384ac63ed21bb0716b2aea2c56750b9181eeb2c
Sriya Reddy Adunur <sadunur@unix1.csc.calpoly.edu>
2023-03-07 23:07:35 -0800

    A working program to solve sudoku puzzles, as specified.

--- sudoku.py
+++ sudoku.py
@@ -3,10 +3,15 @@
 # Given code, Summer '22
 # TODO: Complete this file.
 
+import sys
+import sudoku_grids
+import sudoku_io
 
 def main():
-    pass
-
+    filename = sys.argv[1]
+    grid = sudoku_io.read_grid(filename)
+    filled = sudoku_grids.fill_grid(grid)
+    sudoku_io.print_grid(filled)
 
 if __name__ == "__main__":
     main()
